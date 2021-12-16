f8406baf2c56709262cc29e3ce66c8f1d84b9e2e
Salar Pro <salarkhalid97@gmail.com>
2021-12-16 16:32:47 +0300

    Fist push of codes

---
+++ android/app/src/main/kotlin/com/example/wecode_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.wecode_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/wecode_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Wecode extends StatefulWidget {
+  const Wecode({ Key? key }) : super(key: key);
+
+  @override
+  _WecodeState createState() => _WecodeState();
+}
+
+class _WecodeState extends State<Wecode> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
